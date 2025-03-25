9663b0bc9f09c3e752f465fbc9cbbf16ba571fa6
hayeszm7 <hayeszm7@gmail.com>
2025-03-25 06:50:36 -0400

    added search

--- PokeDex2/Program.cs
+++ PokeDex2/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using PokeDex2;
 using PokeDex2.API;
 using PokeDex2.Models;
@@ -10,6 +11,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ApiClient>();
+//builder.Services.AddDbContext<PokemonDbContext>(options =>
+//    options.UseSqlite("data source=pokemon.db"));
+//builder.Services.AddScoped<PokemonDataService>();
 
 await builder.Build().RunAsync();
 
